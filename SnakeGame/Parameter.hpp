425e59c42b675bceafc3a3a4bb8a563453a2a02e
Ryota1917 <ououaken1917@gmail.com>
2022-08-03 00:52:51 +0900

    プロジェクトの修正

--- SnakeGame/Parameter.hpp
+++ SnakeGame/Parameter.hpp
@@ -3,7 +3,7 @@
 class Parameter {
 public:
 	/// マスの大きさ
-	static const int GridSize = 10;
+	static const int GridSize = 20;
 	/// グリッドの間隔
 	static const int GridSpace = 2;
 	/// ステージの高さ
@@ -11,7 +11,7 @@ public:
 	/// ステージの幅
 	static const int StageWidth = 20;
 	/// 何fpsごとにターンを更新するか
-	static const int TurnSpace = 10;
+	static const int TurnSpace = 5;
 
 	/// Fruitのシード値
 	static const unsigned int FruitSeed = 0x12345679;
