b109ae0974c43eebe21514a51525af802ff7776f
Ryota1917 <ououaken1917@gmail.com>
2022-08-10 02:08:00 +0900

    Update NNSnake.h

--- SnakeGame/NNSnake.h
+++ SnakeGame/NNSnake.h
@@ -3,8 +3,12 @@
 
 class NNSnake :public SnakeActor {
 public:
-	NNSnake(Game* game);
+	NNSnake(Game* game,class StageActor* stage);
 	void UpdateActor(float deltaTime)override;
 private:
 	class StageActor* mStage;
+
+	struct NNState {
+
+	};
 };
